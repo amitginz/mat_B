5b6607d09d9effc35dc0cbe2859118593fa6d167
amitginz <74299421+amitginz@users.noreply.github.com>
2022-03-13 16:33:03 +0200

    upgrade main

--- Main.cpp
+++ Main.cpp
@@ -5,7 +5,12 @@ using namespace std;
 #include <vector>
 #include "mat.hpp"
 int main(){
-    try{
+    cout << "welcome to the carpet factory.."<< endl;
+    cout << "Do yo want to create carpet??Y/N"<< endl;
+    char ans;
+    cin >> ans;
+    if (ans == 'Y'){
+        try{
         int a;
         cout << "please enter number of the wigth of the carpet:" << endl;
         cin >>a;
@@ -22,32 +27,36 @@ int main(){
         cout << ariel::mat(a, b, c, d) << endl;
 
         cout << "succesfuly print carpet..." << endl;
-    }
-    catch (exception){
-        cout<<"mat size is always odd"<< endl;
+        }
+        catch (exception){
+            cout<<"mat size is always odd"<< endl;
         
-    }
+        }
     
-    try{
-        int a;
-        cout << "please enter number of the wigth of the carpet:" << endl;
-        cin >>a;
-        int b;
-        cout << "please enter number of the height of the carpet:" << endl;
-        cin >>b; 
-        char c;
-        cout<< "please enter first simbole of the carpet:" << endl;
-        cin>>c;
-        char d;
-        cout<< "please enter second simbole of the carpet:" << endl;
-        cin >> d;
-        cout << endl;
-        cout << ariel::mat(a, b, c, d) << endl;
+        try{
+            int a;
+            cout << "please enter number of the wigth of the carpet:" << endl;
+            cin >>a;
+            int b;
+            cout << "please enter number of the height of the carpet:" << endl;
+            cin >>b; 
+            char c;
+            cout<< "please enter first simbole of the carpet:" << endl;
+            cin>>c;
+            char d;
+            cout<< "please enter second simbole of the carpet:" << endl;
+            cin >> d;
+            cout << endl;
+            cout << ariel::mat(a, b, c, d) << endl;
 
-        cout << "succesfuly print carpet..." << endl;
+            cout << "succesfuly print carpet..." << endl;
+        }
+        catch (exception){
+            cout<<"mat size is always odd"<< endl;
+        }
     }
-    catch (exception){
-        cout<<"mat size is always odd"<< endl;
+    else{
+        cout<< "Goodbye";
     }
     
 }
